d33de5b21cc3623587c335d31f330ba83abdfd69
aresmardiana01 <108126000+aresmardiana01@users.noreply.github.com>
2022-09-21 10:00:24 +0700

    pembuatan client TCP loman Dep message processing

---
+++ clientLomanAres.js
@@ -0,0 +1,37 @@
+const net = require('net')
+const JsonSocket = require('json-socket')
+
+const socket = new JsonSocket(new net.Socket())
+
+function processMsg(msg) {
+    if (msg.includes('(DEP-')){
+        const pattern = /\(DEP-(?<acid>[A-Z0-9]+)(\/A)?(?<ssr>\d*|)-(?<adep>[A-Z]{4})(?<atd>\d*)-(?<ades>[A-Z]{4})/
+        const result = msg.match(pattern)
+        if (result){
+            console.log(result.groups)
+            console.log('----\n\n')
+        }else{
+            console.log(`Dep message di terima namun tidak sesuai format`)
+            console.log('----\n\n')
+            
+        }
+    }
+}
+
+socket.on('error', err => {
+    console.log(`error on conecting`)
+    console.log(err.message)
+})
+
+socket.on ('connect',()=>{
+    console.log(`connected to loman server`)
+    socket.on('message', message => {
+        // console.log(`message received`) //untuk memunculkan keseluruhan message
+        // console.log(message)
+        // console.log('----\n\n')
+
+        processMsg(message)
+    })
+})
+
+socket.connect(31115,'172.20.10.211')
